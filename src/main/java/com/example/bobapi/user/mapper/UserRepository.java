af895a13b4e87df57fa8db0d3642c637d7750ad8
tt <fj2746@gmail.com>
2023-01-07 12:25:49 +0900

    chore: etc

--- src/main/java/com/example/bobapi/user/mapper/UserRepository.java
+++ src/main/java/com/example/bobapi/user/mapper/UserRepository.java
@@ -3,10 +3,12 @@ package com.example.bobapi.user.mapper;
 import com.example.bobapi.user.vo.SignupRequestVo;
 import com.example.bobapi.user.vo.SignupResultVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserRepository {
 
+    @Select("CALL db_bob.p_user_info_ins(#{email}, #{password}, #{userNick})")
     SignupResultVo pUserInfoIns(SignupRequestVo signupRequestVo);
 
 }
